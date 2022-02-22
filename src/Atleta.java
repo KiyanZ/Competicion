131aaac95a70a6d8c7542160132a210e9c639abb
alu10467008 <alundu008@ieselcaminas.org>
2022-02-22 19:15:01 +0100

    atleta incomplete

--- src/Atleta.java
+++ src/Atleta.java
@@ -1,2 +1,35 @@
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Atleta {
+    private String nombre;
+
+    private enum Genero
+    {
+        MALE, FEMALE
+    }
+
+    private LocalDate Nacimiento;
+    private LocalDate InicioCarrera;
+    private LocalDate FinCarrera;
+
+    public Atleta(String name, Genero sex , LocalDate BirthDate, LocalDate CareerDebut, LocalDate RetirmentDate){
+        nombre = name;
+        Nacimiento = BirthDate;
+        InicioCarrera = CareerDebut;
+        FinCarrera = RetirmentDate;
+
+    }
+
+    public int edad() {
+        return Period.between(Nacimiento, LocalDate.now()).getYears();
+    }
+
+    public boolean debutar(LocalDate date) {
+        if ((Nacimiento == null) || (FinCarrera != null)) {
+            return false;
+        }
+        InicioCarrera = date;
+        return true;
+    }
 }
